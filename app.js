95c1fac8ff31656ae19d6d6c13b89d4b52d1ded7
traag <paulacmpp@gmai.com>
2025-08-14 21:07:25 -0300

    tirando a habilitação

---
+++ app.js
@@ -0,0 +1,21 @@
+alert("Boas vindas ao nosso site!");
+
+let nome = "Lua";
+let idade = 25;
+
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+
+alert(mensagemDeErro);
+
+nome= prompt("Qual é o seu nome?");
+
+idade = parseInt(prompt("Digite a sua idade:")10);
+
+if (idade>=18) {
+    alert("Pode tirar a habilitação!");
+}
